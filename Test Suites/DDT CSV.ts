88c501aacc95c0c6b6844be777a9d92544e0c439
Kornel Urbański <kornel.urbanski@gmail.com>
2024-07-05 11:58:05 +0200

    123

---
+++ Test Suites/DDT CSV.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT CSV</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f8aa5519-140d-4871-b05c-984a46ca284e</testSuiteGuid>
+   <testCaseLink>
+      <guid>7128dcf7-a522-48b1-a240-d11d61026693</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DDTC/Seek for a place</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>38bcc065-cf96-4eae-aba3-449ddebca240</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/CSV_100cities</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>38bcc065-cf96-4eae-aba3-449ddebca240</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>city</value>
+         <variableId>e9f7a3fb-5981-4ae7-8817-d4605239e41a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>38bcc065-cf96-4eae-aba3-449ddebca240</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>result</value>
+         <variableId>25c85899-4e3b-4132-aaf7-b2c42a63a984</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d713e5e9-cb4f-42f5-8063-04ceb52ac76e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
